c14a350ba0cc00b44c7bb34b6a1928b29c5bd27e
Andreev Dmitry <dimandreev@mail.ru>
2020-11-25 09:58:35 +0300

    Editor: context menus moved to the top of the list

--- UnityIntegration/Editor/ContainerAssetMenu.cs
+++ UnityIntegration/Editor/ContainerAssetMenu.cs
@@ -7,22 +7,23 @@ namespace DaSerialization.Editor
 {
     public static class ContainerAssetMenu
     {
-        [MenuItem("Assets/Container/Update Serializers", true)]
-        public static bool SelectionHaveContainers()
+        // priority = 18 - right after 'Create' menu
+        [MenuItem("Assets/Container/Open in Inspector...", priority = 20)]
+        public static void OpenInContainerViewer()
         {
-            foreach (var text in Selection.GetFiltered<TextAsset>(SelectionMode.DeepAssets))
-                if (ContainerRef.FromTextAsset(text).IsValid)
-                    return true;
-            return false;
+            var text = Selection.activeObject as TextAsset;
+            ContainerInspectorWindow.Init();
+            ContainerInspectorWindow window = (ContainerInspectorWindow)EditorWindow.GetWindow(typeof(ContainerInspectorWindow));
+            window.Target = text;
         }
-        [MenuItem("Assets/Container/Open in Viewer...", true)]
+        [MenuItem("Assets/Container/Open in Inspector...", true)]
         public static bool SelectionIsContainer()
         {
             var text = Selection.activeObject as TextAsset;
-            return ContainerRef.FromTextAsset(text).IsValid;
+            return ContainerRef.FromTextAsset(text, false).IsValid;
         }
 
-        [MenuItem("Assets/Container/Update Serializers")]
+        [MenuItem("Assets/Container/Update Serializers", priority = 22)]
         public static void UpdateSerializers()
         {
             int totalCount = 0;
@@ -30,7 +31,7 @@ namespace DaSerialization.Editor
             TextAsset lastUpdated = null;
             foreach (var text in Selection.GetFiltered<TextAsset>(SelectionMode.DeepAssets))
             {
-                if (ContainerRef.FromTextAsset(text).UpdateSerializers())
+                if (ContainerRef.FromTextAsset(text, false).UpdateSerializers())
                     changedCount++;
                 totalCount++;
                 lastUpdated = text;
@@ -38,14 +39,13 @@ namespace DaSerialization.Editor
             Debug.Log($"Serializers updated for {totalCount} containers. {changedCount} changed.\n",
                 totalCount == 1 ? lastUpdated : null);
         }
-
-        [MenuItem("Assets/Container/Open in Viewer...")]
-        public static void OpenInContainerViewer()
+        [MenuItem("Assets/Container/Update Serializers", true)]
+        public static bool SelectionHaveContainers()
         {
-            var text = Selection.activeObject as TextAsset;
-            ContainerInspectorWindow.Init();
-            ContainerInspectorWindow window = (ContainerInspectorWindow)EditorWindow.GetWindow(typeof(ContainerInspectorWindow));
-            window.Target = text;
+            foreach (var text in Selection.GetFiltered<TextAsset>(SelectionMode.DeepAssets))
+                if (ContainerRef.FromTextAsset(text, false).IsValid)
+                    return true;
+            return false;
         }
     }
 }
