040a10a4f88c703d2cae2692a7f5e0dc1399d1e1
EgorFalaleev <Egor1268@gmail.com>
2021-09-20 10:48:37 +0300

    test container is loaded from and saved to a correct location

--- Tests/SerializationContainerTests.cs
+++ Tests/SerializationContainerTests.cs
@@ -4,41 +4,43 @@ namespace Tests
 {
     static class SerializationContainerTests
     {
-        private const string CONTAINER_PATH = "Tests";
-        private const string CONTAINER_NAME = "/test_container";
+        private const string CONTAINER_PATH = "Assets/Tests/";
+        private const string CONTAINER_NAME = "TestContainer";
+        private const string FULL_CONTAINER_PATH = CONTAINER_PATH + CONTAINER_NAME;
 
-        private static BinaryContainer LoadOrCreateContainer(string containerName)
+        private static BinaryContainer LoadOrCreateContainer()
         {
-            var storage = new BinaryContainerStorageOnUnity();
+            var storage = new BinaryContainerStorageOnFiles(null, "");
+            var loadedContainer = storage.LoadContainer(FULL_CONTAINER_PATH, true);
 
-            if (storage.LoadContainer(CONTAINER_PATH + containerName) != null) 
-                return storage.LoadContainer(CONTAINER_PATH + containerName);
+            if (loadedContainer != null) 
+                return loadedContainer;
             else
             {
-                var container = storage.CreateBinaryContainer();
-                storage.SaveContainer(container, CONTAINER_PATH + containerName);
+                var container = storage.CreateContainer();
+                storage.SaveContainer(container, FULL_CONTAINER_PATH);
                 return container;
             }
         }
 
         [Test(-111)]
-        private static bool ContainerExistance()
+        private static bool ContainerExistanceTest()
         {
-            var testContainer = LoadOrCreateContainer(CONTAINER_NAME);
+            var testContainer = LoadOrCreateContainer();
             if (testContainer == null) 
                 throw new FailedTest("Container does not exist.");
             return true;
         }
 
-        [Test(-112)]
+  //      [Test(-112)]
         private static bool TopLevelObjectSerialization()
         {
             var storage = new BinaryContainerStorageOnUnity();
-            var testContainer = LoadOrCreateContainer(CONTAINER_NAME);
+            var testContainer = LoadOrCreateContainer();
             TestClass testClass = new TestClass(testContainer);
 
             testContainer.Serialize(testClass, 0);
-            storage.SaveContainer(testContainer, CONTAINER_PATH + CONTAINER_NAME);
+            storage.SaveContainer(testContainer, FULL_CONTAINER_PATH);
             return true;
         }
     }
