5c86b2561804b3dfd982eb51f2c5b57aafe19dca
EgorFalaleev <Egor1268@gmail.com>
2021-09-21 14:45:51 +0300

    added field meta info to deserializer

--- Tests/SerializationTestCases.cs
+++ Tests/SerializationTestCases.cs
@@ -5,6 +5,7 @@ namespace DaSerialization.Tests
     [Serializable, TypeId(54876)]
     public class TopLevelObject
     {
+
         public int intTest;
         public double doubleTest;
         public short shortTest;
@@ -21,16 +22,18 @@ namespace DaSerialization.Tests
 
     public class TopLevelObjectSerializer : AFullSerializer<TopLevelObject>
     {
-        public override int Version => 1;
+        private const string FIELD_META_INFO_PREFIX = "N_";
+
+        public override int Version => 1;        
 
         public override void ReadDataToObject(ref TopLevelObject obj, BinaryStreamReader reader)
         {
             if (obj == null)
                 obj = new TopLevelObject();
-            obj.intTest = reader.ReadInt32();
-            obj.doubleTest = reader.ReadDouble();
-            obj.shortTest = reader.ReadInt16();
-            obj.stringTest = reader.ReadString();
+            obj.intTest = reader.ReadInt32(FIELD_META_INFO_PREFIX + obj.intTest.PrettyTypeName());
+            obj.doubleTest = reader.ReadDouble(FIELD_META_INFO_PREFIX + obj.doubleTest.PrettyTypeName());
+            obj.shortTest = reader.ReadInt16(FIELD_META_INFO_PREFIX + obj.shortTest.PrettyTypeName());
+            obj.stringTest = reader.ReadString(FIELD_META_INFO_PREFIX + obj.stringTest.PrettyTypeName());
         }
 
         public override void WriteObject(TopLevelObject obj, BinaryStreamWriter writer)
