2ec96d255ca75b64f5d19f51f1b2a9221d589b92
EgorFalaleev <Egor1268@gmail.com>
2021-09-23 09:55:07 +0300

    moved test objects serialization code to CreateContainer()

--- Tests/SerializationContainerTests.cs
+++ Tests/SerializationContainerTests.cs
@@ -10,40 +10,22 @@ namespace DaSerialization.Tests
         private const string FULL_CONTAINER_PATH = CONTAINER_PATH + CONTAINER_NAME;
 
         [MenuItem("Tools/Tests/Create Test Container", priority = int.MaxValue)]
-        private static BinaryContainer CreateContainer()
+        private static void CreateContainer()
         {
             var storage = new BinaryContainerStorageOnFiles(null, "");
             var container = storage.CreateContainer();
+
+            // test object serialization
+            TopLevelObject testObj = new TopLevelObject();
+            container.Serialize(testObj, 0);
+
+            // top level structure serialization
+            TopLevelStructure testStruct = new TopLevelStructure(-1985351954);
+            container.Serialize(testStruct, 1);
+
             storage.SaveContainer(container, FULL_CONTAINER_PATH);
             AssetDatabase.Refresh();
             Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(FULL_CONTAINER_PATH + ".bytes");
-            return container;
-        }
-
-        [Test(-111)]
-        private static bool TopLevelObjectSerializationTest()
-        {
-            var storage = new BinaryContainerStorageOnFiles(null, "");
-            var testContainer = storage.LoadContainer(FULL_CONTAINER_PATH, true);
-            TopLevelObject testObj = new TopLevelObject();
-
-            if (!testContainer.Serialize(testObj, 0))
-                throw new FailedTest($"Failed to serialized {testObj.PrettyTypeName()}");
-            storage.SaveContainer(testContainer, FULL_CONTAINER_PATH);
-            return true;
-        }
-
-        [Test(-112)]
-        private static bool TopLevelStructureSerializationTest()
-        {
-            var storage = new BinaryContainerStorageOnFiles(null, "");
-            var testContainer = storage.LoadContainer(FULL_CONTAINER_PATH, true);
-            TopLevelStructure testStruct = new TopLevelStructure(-1985351954);
-
-            if (!testContainer.Serialize(testStruct, 1))
-                throw new FailedTest($"Failed to serialized {testStruct.PrettyTypeName()}");
-            storage.SaveContainer(testContainer, FULL_CONTAINER_PATH);
-            return true;
         }
     }
 }
