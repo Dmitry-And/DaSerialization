3d31c8c6c5bdab585d9264de4f576333cc97529f
EgorFalaleev <Egor1268@gmail.com>
2021-09-27 12:10:31 +0300

    added top level container serialization

--- Tests/SerializationContainerTests.cs
+++ Tests/SerializationContainerTests.cs
@@ -13,27 +13,22 @@ namespace DaSerialization.Tests
         private static void CreateContainer()
         {
             var storage = new BinaryContainerStorageOnFiles(null, "");
-            var container = storage.CreateContainer();
+            var testContainer = storage.CreateContainer();
 
             // test object serialization
-            TestObject testObj = new TestObject();
-            container.Serialize(testObj, 0);
+            var testObj = new TestObject();
+            testContainer.Serialize(testObj, 0);
 
             // top level structure serialization
-            TopLevelStructure testStruct = new TopLevelStructure(-1985351954);
-            container.Serialize(testStruct, 1);
+            testContainer.Serialize(TopLevelStructure.Default, 1);
 
             // top level container serialization
-            var testContainer = storage.CreateContainer();
-            int data1 = 1576521201;
-            byte data2 = 250;
-            string data3 = "4257-=fefj2";
-            testContainer.Serialize(data1, 0);
-            testContainer.Serialize(data2, 1);
-            testContainer.Serialize(data3, 2);
-            container.Serialize(testContainer, 2);
+            var container = storage.CreateContainer();
+            container.Serialize(testObj, 0);
+            container.Serialize(TopLevelStructure.Default, 1);
+            testContainer.Serialize(container, 2);
 
-            storage.SaveContainer(container, FULL_CONTAINER_PATH);
+            storage.SaveContainer(testContainer, FULL_CONTAINER_PATH);
             AssetDatabase.Refresh();
             Selection.activeObject = AssetDatabase.LoadMainAssetAtPath(FULL_CONTAINER_PATH + ".bytes");
         }
