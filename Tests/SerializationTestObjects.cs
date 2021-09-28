efce46edd96727626c26e57978beb42f46fb8660
EgorFalaleev <Egor1268@gmail.com>
2021-09-28 11:43:47 +0300

    added bottom level structure with serializer

--- Tests/SerializationTestObjects.cs
+++ Tests/SerializationTestObjects.cs
@@ -201,4 +201,99 @@ namespace DaSerialization.Tests
             writer.WriteBytes(obj.BytesTest);
         }
     }
+
+    [Serializable, TypeId(5763879)]
+    public struct BottomLevelStructure
+    {
+        public bool BoolTest;
+        public byte ByteTest;
+        public short ShortTest;
+        public int IntTest;
+        public long LongTest;
+        public sbyte SByteTest;
+        public uint UIntTest;
+        public ulong ULongTest;
+        public decimal DecimalTest;
+        public float FloatTest;
+        public double DoubleTest;
+        public char CharTest;
+        public char CharASCIITest;
+        public string StringTest;
+        public string StringASCIITest;
+        public char[] CharsTest;
+        public char[] CharsASCIITest;
+        public byte[] BytesTest;
+
+        public static BottomLevelStructure Default
+            => new BottomLevelStructure()
+            {
+                BoolTest = true,
+                ByteTest = 252,
+                ShortTest = 29611,
+                IntTest = 1947483603,
+                LongTest = 8002261925743664776,
+                SByteTest = -120,
+                UIntTest = 3921563275,
+                ULongTest = 17576821435769000076,
+                DecimalTest = 4.1222541087m,
+                FloatTest = 2.75557999f,
+                DoubleTest = 3.5484532,
+                CharTest = '-',
+                CharASCIITest = '&',
+                StringTest = "jqpi38vnm//7856",
+                StringASCIITest = "fj2mnz/123/*-",
+                CharsTest = new char[] { '*', '0', 'q' },
+                CharsASCIITest = new char[] { '9', 's', '>' },
+                BytesTest = new byte[] { 7, 100, 252 },
+            };
+    }
+
+    public class BottomLevelStructureSerializer : AFullSerializer<BottomLevelStructure>
+    {
+        public override int Version => 1;
+
+        public override void ReadDataToObject(ref BottomLevelStructure obj, BinaryStreamReader reader)
+        {
+            obj.BoolTest = reader.ReadBool("N_Bool");
+            obj.ByteTest = reader.ReadByte("N_Byte");
+            obj.ShortTest = reader.ReadInt16("N_Int16");
+            obj.IntTest = reader.ReadInt32("N_Int32");
+            obj.LongTest = reader.ReadInt64("N_Int64");
+            obj.SByteTest = reader.ReadSByte("N_SByte");
+            obj.UIntTest = reader.ReadUInt32("N_UInt32");
+            obj.ULongTest = reader.ReadUInt64("N_UInt64");
+            obj.DecimalTest = reader.ReadDecimal("N_Decimal");
+            obj.FloatTest = reader.ReadSingle("N_Single");
+            obj.DoubleTest = reader.ReadDouble("N_Double");
+            obj.CharTest = reader.ReadChar("N_Char");
+            obj.CharASCIITest = reader.ReadCharASCII("N_CharASCII");
+            obj.StringTest = reader.ReadString("N_String");
+            obj.StringASCIITest = reader.ReadStringASCII("N_StringASCII");
+            obj.CharsTest = reader.ReadChars(3, "N_Chars");
+            obj.CharsASCIITest = reader.ReadCharsASCII(3, "N_CharsASCII");
+            obj.BytesTest = reader.ReadBytes(3, "N_Bytes");
+        }
+
+        public override void WriteObject(BottomLevelStructure obj, BinaryStreamWriter writer)
+        {
+            writer.WriteBool(obj.BoolTest);
+            writer.WriteByte(obj.ByteTest);
+            writer.WriteInt16(obj.ShortTest);
+            writer.WriteInt32(obj.IntTest);
+            writer.WriteInt64(obj.LongTest);
+            writer.WriteSByte(obj.SByteTest);
+            writer.WriteUInt32(obj.UIntTest);
+            writer.WriteUInt64(obj.ULongTest);
+            writer.WriteDecimal(obj.DecimalTest);
+            writer.WriteSingle(obj.FloatTest);
+            writer.WriteDouble(obj.DoubleTest);
+            writer.WriteChar(obj.CharTest);
+            writer.WriteCharASCII(obj.CharASCIITest);
+            writer.WriteString(obj.StringTest);
+            writer.WriteStringASCII(obj.StringASCIITest);
+            writer.WriteChars(obj.CharsTest);
+            writer.WriteCharsASCII(obj.CharsASCIITest);
+            writer.WriteBytes(obj.BytesTest);
+        }
+    }
 }
