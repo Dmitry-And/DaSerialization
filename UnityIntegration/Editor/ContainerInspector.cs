3418dd387867052f884e33f13bb4c76636f83b9a
EgorFalaleev <Egor1268@gmail.com>
2021-10-19 15:37:21 +0300

    wip: trying to create custom inspector for TextAsset

--- UnityIntegration/Editor/ContainerInspector.cs
+++ UnityIntegration/Editor/ContainerInspector.cs
@@ -6,18 +6,8 @@ using DaSerialization.Editor;
 [CustomEditor(typeof(TextAsset))]
 public class ContainerInspector : Editor
 {
-    public BinaryContainer ContainerToInspect;
-
     public override void OnInspectorGUI()
     {
-        var containerPath = AssetDatabase.GetAssetPath(target);
-        var containerInfo = new ContainerEditorInfo(ContainerToInspect);
-        var containerView = new ContainerEditorView(containerInfo, ContainerToInspect != null);
-
-        if (containerPath.EndsWith(".bytes"))
-        {
-            var pos = new Rect(new Vector2(), new Vector2(300f, 450f));
-            containerView.Draw(pos);
-        }
+        GUILayout.Label("this works");
     }
 }
