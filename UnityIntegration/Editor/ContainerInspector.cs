6bad5dd5962cb53685c4e6b87b36a5d4db710346
EgorFalaleev <Egor1268@gmail.com>
2021-10-20 16:15:25 +0300

    created custom inspector for DefaultAsset

--- UnityIntegration/Editor/ContainerInspector.cs
+++ UnityIntegration/Editor/ContainerInspector.cs
@@ -3,7 +3,7 @@ using UnityEditor;
 using DaSerialization;
 using DaSerialization.Editor;
 
-[CustomEditor(typeof(TextAsset))]
+[CustomEditor(typeof(DefaultAsset))]
 public class ContainerInspector : Editor
 {
     public override void OnInspectorGUI()
