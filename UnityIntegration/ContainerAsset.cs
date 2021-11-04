1517c79f9cc8257519f49168fb3d5271a6a6c5a8
EgorFalaleev <Egor1268@gmail.com>
2021-11-04 14:49:14 +0300

    added code for trying to get container from byte array

--- UnityIntegration/ContainerAsset.cs
+++ UnityIntegration/ContainerAsset.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
+using DaSerialization;
 
 public class ContainerAsset : Object
 {
+    public byte[] bytes;
 
+    public bool TryGetContainer(byte[] data, out BinaryContainer container, bool verbose = false)
+    {
+        container = UnityStorage.Instance.GetContainerFromData(data, !Application.isPlaying);
+
+        if (container != null)
+            return true;
+        else
+        {
+            if (verbose)
+                Debug.LogError("error");
+            return false;
+        }
+    }
 }
