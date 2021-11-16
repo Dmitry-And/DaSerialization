8ea5a716e3d820ffde93b2a9f99068ed110e2c6a
EgorFalaleev <Egor1268@gmail.com>
2021-11-16 11:04:14 +0300

    wip: inspector for container asset

--- UnityIntegration/Editor/ContainerFilesInspector.cs
+++ UnityIntegration/Editor/ContainerFilesInspector.cs
@@ -3,11 +3,21 @@ using UnityEditor;
 using DaSerialization;
 using DaSerialization.Editor;
 
-[CustomEditor(typeof(DefaultAsset))]
+[CustomEditor(typeof(ContainerAsset))]
 public class ContainerFilesInspector : Editor
 {
+    public ContainerAsset asset;
+    private BinaryContainer container;
+    private ContainerEditorView _containerView;
+
     public override void OnInspectorGUI()
     {
-        GUILayout.Label("this works");
+        var pos = new Rect(new Vector2(), new Vector2(Screen.width, Screen.height));
+        asset = (ContainerAsset)target;
+        asset.TryGetContainer(out container);
+        var info = new ContainerEditorInfo(container);
+        _containerView = new ContainerEditorView(info, true);
+
+        var updatedContainer = _containerView.Draw(pos);
     }
 }
