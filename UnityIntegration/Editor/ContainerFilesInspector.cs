24ad838ef034dde256ab7c8c5cc454fda7065e8f
EgorFalaleev <Egor1268@gmail.com>
2021-11-18 12:07:26 +0300

    added gui code

--- UnityIntegration/Editor/ContainerFilesInspector.cs
+++ UnityIntegration/Editor/ContainerFilesInspector.cs
@@ -6,14 +6,29 @@ using DaSerialization.Editor;
 [CustomEditor(typeof(ContainerAsset))]
 public class ContainerFilesInspector : Editor
 {
+    private static GUIContent RefreshButton = new GUIContent("Refresh", "Reload container from the asset and update all stats");
+
     public ContainerAsset asset;
     private BinaryContainer container;
     private ContainerEditorView _containerView;
 
+    private void Refresh()
+    {
+        _containerView = null;
+        asset = null;
+    }
+
     public override void OnInspectorGUI()
     {
         var pos = new Rect(new Vector2(), new Vector2(Screen.width, Screen.height));
-        asset = (ContainerAsset)target;
+        pos = pos.Expand(-2f);
+        var line = pos.SliceTop();
+        EditorGUI.BeginDisabledGroup(target == null);
+        if (GUI.Button(line.SliceRight(60f), RefreshButton))
+            Refresh();
+        EditorGUI.EndDisabledGroup();
+        EditorGUI.LabelField(line.SliceLeft(38f), "Asset");
+        asset = (ContainerAsset)EditorGUI.ObjectField(line, target, typeof(ContainerAsset), false);
         asset.TryGetContainer(out container);
         var info = new ContainerEditorInfo(container);
         _containerView = new ContainerEditorView(info, true);
